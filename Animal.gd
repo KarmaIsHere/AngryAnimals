366cb1a78cf56cbc89560844c1075a71e841d213
KarmaIsHere <letrixswitch@gmail.com>
2025-12-02 16:55:26 +0200

    Created animal scene and instance

---
+++ Animal.gd
@@ -0,0 +1,83 @@
+extends RigidBody2D
+
+enum AnimalState { Ready, Drag, Release }
+
+@onready var arrow: Sprite2D = $Arrow
+@onready var debug_label: Label = $DebugLabel
+@onready var kick_sound: AudioStreamPlayer2D = $KickSound
+@onready var launch_sound: AudioStreamPlayer2D = $LaunchSound
+
+var _state: AnimalState = AnimalState.Ready
+var _start: Vector2 = Vector2.ZERO
+var _drag_start: Vector2 = Vector2.ZERO
+var _dragged_vector: Vector2 = Vector2.ZERO
+var _arrow_scale_x: float = 0.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	setup()
+	
+
+func setup() -> void:
+	_arrow_scale_x = arrow.scale.x
+	arrow.hide()
+	_start = position
+	
+
+func _physics_process(_delta: float) -> void:
+	update_debug_label()
+
+#region misc
+
+func update_debug_label() -> void:
+	var ds: String = "ST:%s SL:%s FR:%s\n" % [
+		AnimalState.keys()[_state], sleeping, freeze
+	]
+	ds += "_drag_start:%.1f, %.1f\n" % [_drag_start.x, _drag_start.y]
+	ds += "_dragged_vector:%.1f, %.1f" % [_dragged_vector.x, _dragged_vector.y]
+	debug_label.text = ds
+
+#endregion
+
+#region drag
+
+func start_dragging() -> void:
+	arrow.show()
+	_drag_start = get_global_mouse_position()
+
+#endregion
+
+#region state
+
+func change_state(new_state: AnimalState) -> void:
+	if _state == new_state:
+		return
+	
+	_state = new_state
+	
+	match _state:
+		AnimalState.Drag:
+			start_dragging()
+
+
+#endregion
+
+#region signals
+
+func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event.is_action_pressed("drag") and _state == AnimalState.Ready:
+		change_state(AnimalState.Drag)
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	pass # Replace with function body.
+
+
+func _on_sleeping_state_changed() -> void:
+	pass # Replace with function body.
+
+
+func _on_body_entered(body: Node) -> void:
+	pass # Replace with function body.
+	
+#endregion
