22b058997bbbfb8bcd61715881f73f1b72a08964
KarmaIsHere <letrixswitch@gmail.com>
2025-12-03 16:24:54 +0200

    Created player movement

--- Scenes/Animal/Animal.gd
+++ Scenes/Animal/Animal.gd
@@ -2,10 +2,16 @@ extends RigidBody2D
 
 enum AnimalState { Ready, Drag, Release }
 
+const DRAG_LIM_MAX: Vector2 = Vector2(0, 60)
+const DRAG_LIM_MIN: Vector2 = Vector2(-60, 0)
+const IMPULSE_MULT: float = 20.0
+const IMPULSE_MAX: float =  1200.0
+
 @onready var arrow: Sprite2D = $Arrow
 @onready var debug_label: Label = $DebugLabel
 @onready var kick_sound: AudioStreamPlayer2D = $KickSound
 @onready var launch_sound: AudioStreamPlayer2D = $LaunchSound
+@onready var strech_sound: AudioStreamPlayer2D = $StrechSound
 
 var _state: AnimalState = AnimalState.Ready
 var _start: Vector2 = Vector2.ZERO
@@ -13,7 +19,11 @@ var _drag_start: Vector2 = Vector2.ZERO
 var _dragged_vector: Vector2 = Vector2.ZERO
 var _arrow_scale_x: float = 0.0
 
-# Called when the node enters the scene tree for the first time.
+func _unhandled_input(event: InputEvent) -> void:
+	if _state == AnimalState.Drag and event.is_action_released("drag"):
+		change_state(AnimalState.Release)
+		call_deferred("change_state", AnimalState.Release)
+
 func _ready() -> void:
 	setup()
 	
@@ -25,6 +35,7 @@ func setup() -> void:
 	
 
 func _physics_process(_delta: float) -> void:
+	update_state()
 	update_debug_label()
 
 #region misc
@@ -41,13 +52,50 @@ func update_debug_label() -> void:
 
 #region drag
 
+func update_arrow_scale() -> void:
+	var imp_len: float = calculate_impulse().length()
+	var perc: float = clamp(imp_len / IMPULSE_MAX, 0.0, 1.0)
+	arrow.scale.x = lerp(_arrow_scale_x, _arrow_scale_x * 2, perc)
+	arrow.rotation = (_start - position).angle()
+
 func start_dragging() -> void:
 	arrow.show()
 	_drag_start = get_global_mouse_position()
 
+
+func handle_dragging() -> void:
+	var new_drag_vector: Vector2 = get_global_mouse_position() - _drag_start
+	
+	new_drag_vector = new_drag_vector.clamp(DRAG_LIM_MIN, DRAG_LIM_MAX)
+	var diff: Vector2 = new_drag_vector - _dragged_vector
+	
+	if diff.length() > 0 and strech_sound.playing == false:
+		strech_sound.play()
+	_dragged_vector = new_drag_vector
+	position = _start + _dragged_vector
+	update_arrow_scale()
+#endregion
+
+#region release
+
+
+func calculate_impulse() -> Vector2:
+	return _dragged_vector * -IMPULSE_MULT
+
+func start_release() -> void:
+	arrow.hide()
+	launch_sound.play()
+	freeze = false
+	apply_central_impulse(calculate_impulse())
+
 #endregion
 
 #region state
+
+func update_state() -> void:
+	match _state:
+		AnimalState.Drag:
+			handle_dragging()
 
 func change_state(new_state: AnimalState) -> void:
 	if _state == new_state:
@@ -58,6 +106,8 @@ func change_state(new_state: AnimalState) -> void:
 	match _state:
 		AnimalState.Drag:
 			start_dragging()
+		AnimalState.Release:
+			start_release()
 
 
 #endregion
